ea2229708596efa152d11ae1cca61f0b20f67811
Nitesh kumar <nkumar49575@gmail.com>
2025-06-30 07:55:12 +0000

    datatype convert

---
+++ 01_Basic/datatypeconversion.js
@@ -0,0 +1,11 @@
+let score = 33 // number
+console.log(typeof score)
+
+let score1 = "33" // string
+console.log(typeof score1) // agar string ko number me convert karana ho to 
+
+let valueINNumber = Number(score1)
+console.log( typeof valueINNumber)
+
+let score2 = "abc33"
+console.log(score2)
