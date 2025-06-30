b08f468256f972a555536b313eecaaa00affbe98
Nitesh kumar <nkumar49575@gmail.com>
2025-06-30 07:37:56 +0000

    datatype in javascript

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,18 @@
+let name = "Nitesh"
+let age = 18
+let isLoggedIn = false;
+
+
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolen => true/false
+//null => standalone value
+// undefine => 
+// symbol => unique
+
+
+console.log(typeof 18) //number
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
